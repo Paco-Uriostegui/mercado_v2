241fd84fa76730e3618b2cdfa95960170411e216
Francisco UM <pacoum88@gmail.com>
2026-01-27 12:11:51 +0100

    wip: add UserModel mapper in data source

--- lib/features/auth/data/datasources/auth_remote_data_source.dart
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -1,11 +1,13 @@
+import 'package:mercado_v2/features/auth/data/models/user_model.dart';
+
 abstract class AuthRemoteDataSource<T> {
 
-  Future<T?> signInWithEmailAndPassword({
+  Future<UserModel> signInWithEmailAndPassword({
     required String email,
     required String password,
   });
   Future<void> sendPasswordResetEmail({required String email});
-  Future<T?> createUserWithEmailAndPassword({
+  Future<UserModel> createUserWithEmailAndPassword({
     required String email,
     required String password,
   });
--- lib/features/auth/data/datasources/firebase_auth_data_source_impl.dart
+++ lib/features/auth/data/datasources/firebase_auth_data_source_impl.dart
@@ -1,13 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart' as fb;
+import 'package:mercado_v2/core/result/failure.dart';
 import 'package:mercado_v2/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:mercado_v2/features/auth/data/models/user_model.dart';
 
-class FirebaseAuthDataSourceImpl implements AuthRemoteDataSource{
+class FirebaseAuthDataSourceImpl implements AuthRemoteDataSource {
   final fb.FirebaseAuth _firebaseAuth;
 
-  FirebaseAuthDataSourceImpl({required fb.FirebaseAuth firebaseAuth}) : _firebaseAuth = firebaseAuth;
-  
+  FirebaseAuthDataSourceImpl({required fb.FirebaseAuth firebaseAuth})
+    : _firebaseAuth = firebaseAuth;
+
   @override
-  Future<fb.User?> signInWithEmailAndPassword({
+  Future<UserModel> signInWithEmailAndPassword({
     required String email,
     required String password,
   }) async {
@@ -15,18 +18,21 @@ class FirebaseAuthDataSourceImpl implements AuthRemoteDataSource{
       email: email,
       password: password,
     );
-    return userCredential.user;
+
+    final user = userCredential.user;
+    if (user == null) {
+      throw AuthException('User is null after signIn');
+    }
+    return UserModel.fromFirebaseUser(user);
   }
 
   @override
-  Future<void> sendPasswordResetEmail({
-    required String email,
-  }) async {
+  Future<void> sendPasswordResetEmail({required String email}) async {
     await _firebaseAuth.sendPasswordResetEmail(email: email);
   }
 
   @override
-  Future<fb.User?> createUserWithEmailAndPassword({
+  Future<UserModel> createUserWithEmailAndPassword({
     required String email,
     required String password,
   }) async {
@@ -34,8 +40,13 @@ class FirebaseAuthDataSourceImpl implements AuthRemoteDataSource{
       email: email,
       password: password,
     );
-    return userCredential.user;
+     final user = userCredential.user;
+    if (user == null) {
+      throw AuthException('User is null after signIn');
+    }
+    return UserModel.fromFirebaseUser(user);
   }
+
   @override
   Future<void> sendEmailVerification() async {
     await _firebaseAuth.currentUser?.sendEmailVerification();
@@ -46,7 +57,7 @@ class FirebaseAuthDataSourceImpl implements AuthRemoteDataSource{
     await _firebaseAuth.currentUser?.reload();
     return _firebaseAuth.currentUser?.emailVerified ?? false;
   }
-  
+
   @override
   fb.User? getCurrentUser() {
     return _firebaseAuth.currentUser;
