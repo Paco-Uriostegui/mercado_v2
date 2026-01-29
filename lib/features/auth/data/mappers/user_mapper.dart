6a365511365fb5ac979518f93359182f594915ae
Paco-Uriostegui <pacoum88@gmail.com>
2026-01-29 07:34:32 +0100

    wip: login bloc

--- lib/features/auth/data/mappers/user_mapper.dart
+++ lib/features/auth/data/mappers/user_mapper.dart
@@ -1,14 +1,13 @@
-
-
-
-
 import 'package:firebase_auth/firebase_auth.dart' as fb;
+import 'package:mercado_v2/core/result/failure.dart';
 
 import '../../domain/entities/user.dart';
 
 class UserMapper {
-  
   User fromFirebase(fb.User firebaseUser) {
+    if (firebaseUser.uid.isEmpty) {
+      throw AuthException('firebaseUser id is empty');
+    }
     return User(uid: firebaseUser.uid);
   }
 }
