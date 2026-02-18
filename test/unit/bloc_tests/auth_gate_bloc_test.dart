b555c4dba14f1c433aac636fc86e2d59be6f9756
Francisco UM <pacoum88@gmail.com>
2026-02-18 12:20:56 +0100

    p wip: auth gate bloc tests

--- test/unit/bloc_tests/auth_gate_bloc_test.dart
+++ test/unit/bloc_tests/auth_gate_bloc_test.dart
@@ -1,21 +1,42 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mercado_v2/features/auth/email_fb_authentication/domain/repositories/iauth_gate.dart';
 import 'package:mercado_v2/features/auth/email_fb_authentication/domain/usecases/authenticate_usecase.dart';
 import 'package:mercado_v2/features/auth/email_fb_authentication/presentation/blocs/auth_gate/auth_gate_bloc.dart';
+import 'package:mercado_v2/features/auth/email_fb_authentication/presentation/blocs/auth_gate/auth_gate_event.dart';
+import 'package:mercado_v2/features/auth/email_fb_authentication/presentation/blocs/auth_gate/auth_gate_state.dart';
 import 'package:mockito/mockito.dart';
 
 import '../../mocks/mocks.mocks.dart';
 
 void main() {
+  late MockAuthenticationUseCase mockUseCase;
   group("AuthGate bloc tests", () {
-    late AuthGateBloc bloc;
-    late MockAuthenticationUseCase mockUseCase;
     setUp(() {
       mockUseCase = MockAuthenticationUseCase();
-      bloc = AuthGateBloc(mockUseCase);
-
-      // ----------------------------------- stubs
-      when(mockUseCase.call()).thenAnswer((_) => Stream<AuthUserStatus>.value(AuthUserComplete()));
     });
+    blocTest<AuthGateBloc, AuthGateState>(
+      "Test 3: Emits [Loading, goToLogin] when AuthUser is null",
+      build: () {
+        when(
+          mockUseCase.call(),
+        ).thenAnswer((_) => Stream<AuthUserStatus>.value(AuthUserUnlogged()));
+        return AuthGateBloc(mockUseCase);
+      },
+
+      act: (bloc) => bloc.add(InitStream()),
+
+      expect: () =>
+        [
+          AuthGateState.loading(),
+          AuthGateState.goToLogin(),
+        ]
+        //expect(AuthGateState, AuthGateState.goToLogin());
+      ,
+      verify: (_) {
+        verify(mockUseCase).called(1);
+      },
+    );
     //
   });
 }
